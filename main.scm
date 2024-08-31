4e6a48024326e9c609c13e73b61fd9881d4c823e
aikoschurmann <aiko.schurmann@gmail.com>
2024-08-31 19:28:07 +0200

    cleanup

--- main.scm
+++ main.scm
@@ -8,25 +8,38 @@
 (load "initializers.scm")
 (load "neuralnet.scm")
 
-
-
+;todo add optimisers now gradient descent 
+;eg: ADA ADAM etc
 
 (define layers (list (create-layer 2 10 (make-activation ReLU derReLU) XavierInitializer)
                      (create-layer 10 10 (make-activation ReLU derReLU) XavierInitializer)
                      (create-layer 10 1 (make-activation Sigmoid derSigmoid) XavierInitializer)))
 
 (define network (create-FFNN layers (make-error mean-squared-error derivative-mean-squared-error)))
-(define dataset (list (cons #(1 0) #(1))
-                      (cons #(1 1) #(0))
-                      (cons #(0 0) #(0))
-                      (cons #(0 1) #(1))))
 
-(network 'train dataset 1000)
+(define xor-dataset
+  (list (cons #(0 0) #(0))
+        (cons #(0 1) #(1))
+        (cons #(1 0) #(1))
+        (cons #(1 1) #(0))))
+        
+(define circle-dataset (generate-circle-dataset 100))
+(define spiral-dataset (generate-spiral-dataset 100))
 
-(display (network 'forward #(#(1 0))))
+; Define the layers and network
+(define layers (list (create-layer 2 10 (make-activation ReLU derReLU) XavierInitializer)
+                     (create-layer 10 10 (make-activation ReLU derReLU) XavierInitializer)
+                     (create-layer 10 1 (make-activation Sigmoid derSigmoid) XavierInitializer)))
+
+(define network (create-FFNN layers (make-error mean-squared-error derivative-mean-squared-error)))
+
+(network 'train circle-dataset 1000)
+
+; Test the network with a few example inputs
+(display (network 'forward #( #(0.1 0.1) )))
 (newline)
-(display (network 'forward #(#(1 1))))
+(display (network 'forward #( #(0.5 0.5) )))
 (newline)
-(display (network 'forward #(#(0 0))))
+(display (network 'forward #( #(0.9 0.9) )))
 (newline)
-(display (network 'forward #(#(0 1))))
+(display (network 'forward #( #(0.1 0.9) )))
--- math.scm
+++ math.scm
@@ -27,3 +27,31 @@
          (minutes (quotient (round total-seconds) 60))
          (seconds (remainder (round total-seconds) 60)))
     (string-append (number->string minutes) "m:" (number->string seconds) "s")))
+
+(define (generate-circle-dataset n)
+  (define (point-in-circle? x y)
+    (< (+ (* (- x 0.5) (- x 0.5)) (* (- y 0.5) (- y 0.5))) (* 0.5 0.5)))
+  (define (helper i dataset)
+    (if (= i 0)
+        dataset
+        (let* ((x (random))
+               (y (random))
+               (label (if (point-in-circle? x y) #(1) #(0))))
+          (helper (- i 1) (cons (cons (vector x y) label) dataset)))))
+  (helper n '()))
+      
+(define (generate-spiral-dataset n)
+  (define (generate-spiral-class t sign)
+    (let* ((r (* 0.5 t))
+           (x (+ 0.5 (* r (cos (* sign t 4 pi)))))
+           (y (+ 0.5 (* r (sin (* sign t 4 pi))))))
+      (cons (vector x y) (vector (if (= sign 1) 1 0)))))
+  (define (helper i dataset)
+    (if (= i 0)
+        dataset
+        (let* ((t (/ i n))
+               (sign (if (even? i) 1 -1))
+               (point (generate-spiral-class t sign)))
+          (helper (- i 1) (cons point dataset)))))
+  (helper n '()))
+      
